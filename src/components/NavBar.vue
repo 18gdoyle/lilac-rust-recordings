772c4349ef24174fab57c1723b132d1ae9c6c6d1
Greg Doyle <83316865+18gdoyle@users.noreply.github.com>
2024-09-02 18:07:55 -0400

    subtle updates

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,231 +1,196 @@
 <template>
-    <div class="menu-items">
-        <li><a href="/">Home</a></li>
-        <li><a href="/instrumentals">Instrumentals</a></li>
-        <li><a href="/studioservices">Studio Services</a></li>
-        <li><a href="/pastprojects">Past Projects</a></li>
-        <li><a href="/webdesign">Web Design</a></li>
-        <li><a href="/lilacrustmusic">My Music</a></li>
-        <li><a href="/about">Contact Info</a></li>
+  <div class="menu-items">
+    <li><a href="/">Home</a></li>
+    <li><a href="/instrumentals">Instrumentals</a></li>
+    <li><a href="/studioservices">Studio Services</a></li>
+    <li><a href="/pastprojects">Past Projects</a></li>
+    <li><a href="/webdesign">Web Design</a></li>
+    <li><a href="/lilacrustmusic">My Music</a></li>
+    <li><a href="/about">Contact Info</a></li>
+  </div>
+  <div id="navBar" class="navbar">
+    <div class="nav-container">
+      <input
+        @click="showOrHideMobileNav"
+        class="checkbox"
+        type="checkbox"
+        name=""
+        id=""
+      />
+      <div class="hamburger-lines">
+        <span class="line line1"></span>
+        <span class="line line2"></span>
+        <span class="line line3"></span>
+      </div>
     </div>
-    <div id="navBar" class="navbar">
-        <div class="nav-container">
-            <input @click="showOrHideMobileNav" class="checkbox" type="checkbox" name="" id="" />
-            <div class="hamburger-lines">
-                <span class="line line1"></span>
-                <span class="line line2"></span>
-                <span class="line line3"></span>
-            </div>
-        </div>
-        <div id="title">
-            <a href="/" class="hover-underline-animation" v-on:click.ctrl="$router.push('/instrumentalsparse');">Lilac Rust</a>
-        </div>
-        <div id="pages">
-            <div class="pageOption">
-                <a href="/instrumentals" class="hover-underline-animation">Instrumentals</a>
-            </div>
-            <div class="pageOption">
-                <a href="/studioservices" class="hover-underline-animation">Studio Services</a>
-            </div>
-            <div class="pageOption">
-                <a href="/pastprojects" class="hover-underline-animation">Past Projects</a>
-            </div>
-            <div class="pageOption">
-                <a href="/webdesign" class="hover-underline-animation">Web Design</a>
-            </div>
-            <div class="pageOption">
-                <a href="/lilacrustmusic" class="hover-underline-animation">My Music</a>
-            </div>
-            <div class="pageOption">
-                <a href="/about" class="hover-underline-animation">About</a>
-            </div>
-        </div>
+    <div id="title">
+      <a
+        href="/"
+        class="hover-underline-animation"
+        v-on:click.ctrl="$router.push('/instrumentalsparse')"
+        >Lilac Rust</a
+      >
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        showOrHideMobileNav() {
-            let menuItems = document.getElementsByClassName('menu-items')[0];
-            if (menuItems.style.transform === '') {
-                menuItems.style.transform = 'translateX(0)';
-            } else {
-                menuItems.style.transform = '';
-            }
-        }
-    }
-}
+  methods: {
+    showOrHideMobileNav() {
+      let menuItems = document.getElementsByClassName("menu-items")[0];
+      if (menuItems.style.transform === "") {
+        menuItems.style.transform = "translateX(0)";
+      } else {
+        menuItems.style.transform = "";
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-.nav-container {
-    display: none;
-}
-
-.menu-items {
-    display: none;
-}
-
 #navBar {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: max(8vh, 50px);
-    background: rgba(0, 0, 0, 0.5);
-    backdrop-filter: blur(10px);
-    -webkit-backdrop-filter: blur(5px);
-    display: flex;
-    align-items: center;
-    z-index: 10;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: max(8vh, 50px);
+  background: rgba(0, 0, 0, 0.5);
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(5px);
+  display: flex;
+  align-items: center;
+  z-index: 10;
 }
 
 #title {
-    color: white;
-    font-size: xx-large;
-    padding-left: 1%;
-    width: 30%;
-    height: 100%;
-    display: flex;
-    align-items: center;
+  color: white;
+  font-size: xx-large;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: right;
+  padding-right: 20px;
+  width: 100%;
 }
 
-#pages {
-    height: 100%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: right;
-    padding-right: 1%;
+.nav-container {
+  display: block;
+  position: relative;
+  height: 60px;
 }
 
-.pageOption {
-    padding: 1%;
-    color: white;
-    font-size: large;
+.menu-items {
+  display: flex;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
+
+.nav-container .checkbox {
+  position: absolute;
+  display: block;
+  height: 32px;
+  width: 32px;
+  top: 20px;
+  left: 20px;
+  z-index: 5;
+  opacity: 0;
+  cursor: pointer;
+}
+
+.nav-container .hamburger-lines {
+  display: block;
+  height: 26px;
+  width: 32px;
+  position: absolute;
+  top: 17px;
+  left: 20px;
+  z-index: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.nav-container .hamburger-lines .line {
+  display: block;
+  height: 4px;
+  width: 100%;
+  border-radius: 10px;
+  background: white;
+}
+
+.nav-container .hamburger-lines .line1 {
+  transform-origin: 0% 0%;
+  transition: transform 0.4s ease-in-out;
+}
+
+.nav-container .hamburger-lines .line2 {
+  transition: transform 0.2s ease-in-out;
+}
+
+.nav-container .hamburger-lines .line3 {
+  transform-origin: 0% 100%;
+  transition: transform 0.4s ease-in-out;
+}
+
+.nav-container input[type="checkbox"]:checked ~ .menu-items {
+  transform: translateX(0);
+}
+
+.nav-container input[type="checkbox"]:checked ~ .hamburger-lines .line1 {
+  transform: rotate(45deg);
+}
+
+.nav-container input[type="checkbox"]:checked ~ .hamburger-lines .line2 {
+  transform: scaleY(0);
+}
+
+.nav-container input[type="checkbox"]:checked ~ .hamburger-lines .line3 {
+  transform: rotate(-45deg);
+}
+
+.nav-container input[type="checkbox"]:checked ~ .logo {
+  display: none;
+}
+
+.menu-items {
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: max(8vh, 50px);
+  background: rgba(0, 0, 0, 0.7);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(15px);
+  height: 100vh;
+  width: 20vw;
+  transform: translate(-150%);
+  display: flex;
+  flex-direction: column;
+  padding-left: 20px;
+  transition: transform 0.5s ease-in-out;
+  z-index: 10;
+}
+
+.menu-items li {
+  margin-bottom: 1rem;
+  font-size: 1.5rem;
+  font-weight: 500;
 }
 
 @media (max-width: 850px) {
-    #pages {
-        display: none;
-    }
-
-    #title {
-        justify-content: right;
-        padding-right: 5%;
-        width: 100%;
-    }
-
-    .nav-container {
-        display: block;
-        position: relative;
-        height: 60px;
-    }
-
-    .menu-items {
-        display: flex;
-    }
-
-    li {
-        list-style: none;
-    }
-
-    a {
-        text-decoration: none;
-        color: white;
-    }
-
-    .nav-container .checkbox {
-        position: absolute;
-        display: block;
-        height: 32px;
-        width: 32px;
-        top: 20px;
-        left: 20px;
-        z-index: 5;
-        opacity: 0;
-        cursor: pointer;
-    }
-
-    .nav-container .hamburger-lines {
-        display: block;
-        height: 26px;
-        width: 32px;
-        position: absolute;
-        top: 17px;
-        left: 20px;
-        z-index: 2;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-    }
-
-    .nav-container .hamburger-lines .line {
-        display: block;
-        height: 4px;
-        width: 100%;
-        border-radius: 10px;
-        background: white;
-    }
-
-    .nav-container .hamburger-lines .line1 {
-        transform-origin: 0% 0%;
-        transition: transform 0.4s ease-in-out;
-    }
-
-    .nav-container .hamburger-lines .line2 {
-        transition: transform 0.2s ease-in-out;
-    }
-
-    .nav-container .hamburger-lines .line3 {
-        transform-origin: 0% 100%;
-        transition: transform 0.4s ease-in-out;
-    }
-
-    .nav-container input[type="checkbox"]:checked~.menu-items {
-        transform: translateX(0);
-    }
-
-    .nav-container input[type="checkbox"]:checked~.hamburger-lines .line1 {
-        transform: rotate(45deg);
-    }
-
-    .nav-container input[type="checkbox"]:checked~.hamburger-lines .line2 {
-        transform: scaleY(0);
-    }
-
-    .nav-container input[type="checkbox"]:checked~.hamburger-lines .line3 {
-        transform: rotate(-45deg);
-    }
-
-    .nav-container input[type="checkbox"]:checked~.logo {
-        display: none;
-    }
-
-    .menu-items {
-        position: fixed;
-        top: 0;
-        left: 0;
-        padding-top: max(8vh, 50px);
-        background: rgba(0, 0, 0, 0.7);
-        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
-        backdrop-filter: blur(15px);
-        height: 100vh;
-        width: 60vw;
-        transform: translate(-150%);
-        display: flex;
-        flex-direction: column;
-        padding-left: 20px;
-        transition: transform 0.5s ease-in-out;
-        z-index: 10;
-    }
-
-    .menu-items li {
-        margin-bottom: 1rem;
-        font-size: 1.5rem;
-        font-weight: 500;
-    }
-
+  #title {
+    padding-right: 5%;
+  }
+  .menu-items {
+    width: 60vw;
+  }
 }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,37 +2,68 @@
   <div class="about pageTopMargin">
     <div class="glassDiv">
       <div class="imgAndText">
-        <img alt="Lilac Rust Profile Photo" src="/lilacProfileWithFace.jpg" class="contactCard-img" />
-        <p class="aboutDescription">
-          Hey there!
-          My name is Greg, but I go by Lilac Rust online.
-          I'm a multi-instrumentalist music producer and audio engineer. I've been making music for almost 10 years now
-          and I've produced everything from acoustic ballads to trap bangers, cinematic atmospheres to indie funk. I can
-          produce instrumentals for a myriad of genres, as well as record, mix, master, really whatever you need!
-          Chances are, if you are doing something
-          related to music, I can probably help you out. Just hit me up using one of the links below
-          and we'll chat.
+        <img
+          alt="Lilac Rust Profile Photo"
+          src="/lilacProfileWithFace.jpg"
+          class="contactCard-img"
+        />
+        <div class="aboutDescription">
+          Hey there! My name is Greg, but I go by Lilac Rust online. I'm a
+          multi-instrumentalist music producer and audio engineer. I've been
+          making music for almost 10 years now and I've produced everything from
+          acoustic ballads to trap bangers, cinematic atmospheres to indie funk.
+          I can produce instrumentals for a myriad of genres, as well as record,
+          mix, master, really whatever you need! Chances are, if you are doing
+          something related to music, I can probably help you out. Just hit me
+          up using one of the links below and we'll chat.
           <br />
+          ~
           <br />
-          - Greg, a.k.a Lilac Rust
-        </p>
+          Greg, a.k.a Lilac Rust
+          <br />
+            <a
+            class="hover-underline-animation emailLink"
+            href="mailto:lilac.rust@gmail.com"
+            target="_blank"
+            >lilac.rust@gmail.com</a
+          >
+        </div>
       </div>
       <br />
       <div class="iconsRow">
-        <a class="socialMediaButton" href="https://www.tiktok.com/@lilac_rust" target="_blank"><i
-            class="fa-brands fa-tiktok socialMediaIcon"></i></a>
-        <a class="socialMediaButton" href="mailto:lilac.rust@gmail.com" target="_blank"><i
-            class="fa-regular fa-envelope socialMediaIcon"></i></a>
-        <a class="socialMediaButton" href="https://www.instagram.com/lilac_rust_music/" target="_blank"><i
-            class="fa-brands fa-instagram socialMediaIcon"></i></a>
+        <a
+          class="socialMediaButton"
+          href="https://www.tiktok.com/@lilac_rust"
+          target="_blank"
+          ><i class="fa-brands fa-tiktok socialMediaIcon"></i
+        ></a>
+        <a
+          class="socialMediaButton"
+          href="mailto:lilac.rust@gmail.com"
+          target="_blank"
+          ><i class="fa-regular fa-envelope socialMediaIcon"></i
+        ></a>
+        <a
+          class="socialMediaButton"
+          href="https://www.instagram.com/lilac_rust_music/"
+          target="_blank"
+          ><i class="fa-brands fa-instagram socialMediaIcon"></i
+        ></a>
       </div>
       <br />
       <div class="iconsRow">
-        <a class="socialMediaButton" href="https://www.youtube.com/@lilacrust8805" target="_blank"><i
-            class="fa-brands fa-youtube socialMediaIcon"></i></a>
-        <a class="socialMediaButton"
-          href="https://open.spotify.com/artist/1YT4RD3frRo0r9jVY9mBB5?si=jTH5pF9NRJKeDGDx5sNNuw" target="_blank"><i
-            class="fa-brands fa-spotify socialMediaIcon"></i></a>
+        <a
+          class="socialMediaButton"
+          href="https://www.youtube.com/@lilacrust8805"
+          target="_blank"
+          ><i class="fa-brands fa-youtube socialMediaIcon"></i
+        ></a>
+        <a
+          class="socialMediaButton"
+          href="https://open.spotify.com/artist/1YT4RD3frRo0r9jVY9mBB5?si=jTH5pF9NRJKeDGDx5sNNuw"
+          target="_blank"
+          ><i class="fa-brands fa-spotify socialMediaIcon"></i
+        ></a>
       </div>
       <br />
     </div>
@@ -42,6 +73,11 @@
 </template>
 
 <style scoped>
+.emailLink {
+  width: fit-content;
+  font-size: x-large;
+  display: inline;
+}
 .centered {
   display: flex;
   width: 100%;
@@ -98,7 +134,7 @@
   margin: auto;
   display: inline;
   width: 20%;
-  box-shadow: 20px 20px 22px rgba(0, 0, 0, .2);
+  box-shadow: 20px 20px 22px rgba(0, 0, 0, 0.2);
 }
 
 .glassDiv {
@@ -122,6 +158,7 @@
     align-items: center;
     justify-content: center;
     text-align: center;
+    flex-direction: column;
   }
 
   .imgAndText {
